1cd19b4ade820e8376d5f8d083753aeb0b0e79bb
Sam-Umbra <samuuel2008@hotmail.com>
2025-04-11 10:00:31 -0300

    Fase 1 - Add endpoint /clientes - Add classe cliente - Add classe clienteController

---
+++ src/test/java/br/dev/umbra/OSApiApplication/OsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.umbra.OSApiApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
