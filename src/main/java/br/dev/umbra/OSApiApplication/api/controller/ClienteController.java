32f2bb83d466859526f9393dddbeb5cfad54ce47
Sam-Umbra <samuuel2008@hotmail.com>
2025-04-11 11:25:47 -0300

    Fase 4 - Metodo POST - Update ClienteController    - Enpoint POST /clientes    - Function adicionar

--- src/main/java/br/dev/umbra/OSApiApplication/api/controller/ClienteController.java
+++ src/main/java/br/dev/umbra/OSApiApplication/api/controller/ClienteController.java
@@ -9,9 +9,13 @@ import br.dev.umbra.OSApiApplication.domain.repository.ClienteRepository;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -41,6 +45,14 @@ public class ClienteController {
         } else {
             return ResponseEntity.notFound().build();
         }
+        
+    }
+    
+    @PostMapping("/clientes")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente adicionar(@RequestBody Cliente cliente) {
+        
+        return clienteRepository.save(cliente);
         
     }
     
